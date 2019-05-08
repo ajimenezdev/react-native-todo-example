6297f0e87555751472abf75a4f7218f77c0c69b4
ajimenezdev <ajmjimens@gmail.com>
2019-05-08 23:53:42 +0200

    Mover HolaMundo a un component separado como pantalla

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import TodoList from "./screens/main";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hola Mundo!</Text>
-      </View>
-    );
+    return <TodoList />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});

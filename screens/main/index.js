702a8124fe78278c5d123a39ce0c85e3e8621698
ajimenezdev <ajmjimens@gmail.com>
2019-05-09 08:03:22 +0200

    Contenido leccion JSX y Variables

--- screens/main/index.js
+++ screens/main/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+const tasks = [
+  {
+    text: "Tarea 1",
+    done: false
+  },
+  {
+    text: "Tarea 2",
+    done: false
+  },
+  {
+    text: "Tarea 3",
+    done: false
+  },
+  {
+    text: "Tarea 4",
+    done: false
+  },
+  {
+    text: "Nueva",
+    done: false
+  }
+];
+
 class TodoList extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text selectable>Todo List App</Text>
-        <Text>Tarea 1</Text>
-        <Text>Tarea 2</Text>
-        <Text>Tarea multiples {"\n"} lineas</Text>
+        {tasks.map(task => !task.done && <Text>{task.text}</Text>)}
       </View>
     );
   }

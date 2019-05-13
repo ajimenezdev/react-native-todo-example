4ffe0101694f969f52caa560f09317d05e428fdc
ajimenezdev <ajmjimens@gmail.com>
2019-05-13 22:09:46 +0200

    Configurar StatusBar light theme

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import AppNavigator from "./navigation/AppNavigator";
 
 export default class App extends React.Component {
   render() {
-    return <AppNavigator />;
+    return (
+      <React.Fragment>
+        <StatusBar barStyle="light-content" />
+        <AppNavigator />
+      </React.Fragment>
+    );
   }
 }
